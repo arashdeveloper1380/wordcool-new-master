1ae791cd0a86b95bceef8718a8cf2619ac28738a
arash narimani <arash.developer1380@gmail.com>
2023-10-26 12:37:54 +0330

    config and run blade

--- app/http/controllers/Home/HomeController.php
+++ app/http/controllers/Home/HomeController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use Core\Request\Request;
+use Core\View\View;
 
 class HomeController extends Controller{
 
     public function index(){
         // $req = (new Request())->all();
-        dd(request()->all());
+        // dd(request()->all());
+        $name = "arash";
+        return View::blade('index', compact('name'));
         
     }
 
--- core/View/VIew.php
+++ core/View/VIew.php
@@ -12,13 +12,14 @@ use Illuminate\Filesystem\Filesystem;
 class View {
 
     private static $viewFactory;
+
     public static function blade($view, $data = [])
     {
         if (!isset(self::$viewFactory)) {
-            $viewPaths = [BASE_PATH . 'resources/views'];
+            $viewPaths = [BASE_PATH . '/resources/views'];
             $fileSystem = new Filesystem;
             $viewFinder = new FileViewFinder($fileSystem, $viewPaths);
-            $bladeCompiler = new BladeCompiler($fileSystem, BASE_PATH . 'resources/cache/views');
+            $bladeCompiler = new BladeCompiler($fileSystem, BASE_PATH . '/resources/cache/views');
             $resolver = new EngineResolver;
             $resolver->register('blade', function () use ($bladeCompiler) {
                 return new CompilerEngine($bladeCompiler);
