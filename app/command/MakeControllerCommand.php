13d2fbd059dc24d9c3355faeee84542d54e5059a
arash narimani <arash.developer1380@gmail.com>
2023-11-02 13:50:42 +0330

    add stub template

--- app/command/MakeControllerCommand.php
+++ app/command/MakeControllerCommand.php
@@ -16,14 +16,33 @@ class MakeControllerCommand extends Command
         $this
             ->setDescription('make controller file')
             ->setHelp('This command allows you to generate a new contoller file.')
-            ->addArgument('filename', InputArgument::REQUIRED, 'the name of the controller to create');
+            ->addArgument('controllername', InputArgument::REQUIRED, 'the name of the controller to create')
+            ->addArgument('path', InputArgument::OPTIONAL, 'The path to create the file', './');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $controllerName = $input->getArgument('controllername');
+        $path = $input->getArgument('path');
+
+        $filePath = __DIR__ . '/../http/controllers/' . $path . '/' . $controllerName . '.php';
+        $stubPath = __DIR__ . '/stubs/Controller.stub';
+        $fileContent = file_get_contents($stubPath);
+
+        $fileContent = str_replace('{{className}}', $controllerName, $fileContent);
+
+        // if(!empty($path)){
+        //     $fileContent = str_replace('{{dir}}', $path, $fileContent);
+        // }
+        
+        
+        file_put_contents($filePath, $fileContent);
+
         $output->writeln([
+            '',
             '[*] Controller Created Successfully',
             '====================================',
+            'File Path: ' . realpath($filePath),
             '',
         ]);
         return Command::SUCCESS;
