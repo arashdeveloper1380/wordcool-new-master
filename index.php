2f1282cd05dbec649661d53651669de743d253cb
arash narimani <arash.developer1380@gmail.com>
2023-11-05 15:05:45 +0330

    fix bug think

--- index.php
+++ index.php
@@ -8,6 +8,5 @@ require_once 'vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-require_once 'think';
 require_once __DIR__ . '/config/database.php';
 require_once 'bootstarp/boot.php';
