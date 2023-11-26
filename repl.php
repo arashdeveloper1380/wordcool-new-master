e74d538ceee3ee0bbc0d31c91917d8c53ada46cf
arashdeveloper1380 <arash.developer1380@gmail.com>
2023-11-26 21:40:38 +0330

    fix bug database not found

--- repl.php
+++ repl.php
@@ -2,6 +2,11 @@
 
 require 'vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+require_once __DIR__ . './config/database.php';
+
 use Psy\Shell;
 
 $shell = new Shell();
